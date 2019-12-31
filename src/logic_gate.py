f4c2c3eeb99f37ef626e32fdec283a855318c88c
jhpark <mugenjh@gmail.com>
2019-12-31 22:49:07 +0900

    Add XOR gate

--- src/logic_gate.py
+++ src/logic_gate.py
@@ -25,6 +25,12 @@ def or_gate(x1, x2):
     return int(tmp > 0)
 
 
+def xor_gate(x1, x2):
+    s1 = nand_gate(x1, x2)
+    s2 = or_gate(x1, x2)
+    return and_gate(s1, s2)
+
+
 print(and_gate(0, 0))
 print(and_gate(1, 0))
 print(and_gate(0, 1))
@@ -38,4 +44,9 @@ print(nand_gate(1, 1), end='\n\n')
 print(or_gate(0, 0))
 print(or_gate(1, 0))
 print(or_gate(0, 1))
-print(or_gate(1, 1))
+print(or_gate(1, 1), end='\n\n')
+
+print(xor_gate(0, 0))
+print(xor_gate(1, 0))
+print(xor_gate(0, 1))
+print(xor_gate(1, 1))
