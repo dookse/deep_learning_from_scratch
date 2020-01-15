69e296cba23d1a9d5604431fb371df62143e564e
jhpark <mugenjh@gmail.com>
2020-01-15 21:38:38 +0900

    softmax

--- src/softmax.py
+++ src/softmax.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 
-def softmapx(a):
-    exp_a = np.exp(a)
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
+
+
+if __name__ == '__main__':
+    print(softmax(np.array([1010, 1000, 990])))
