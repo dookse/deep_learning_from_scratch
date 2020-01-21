9459d7e5403b59aa1aa31068fbbd00af918b0030
jhpark <mugenjh@gmail.com>
2020-01-21 23:22:24 +0900

    cross entropy mini batch

--- src/cross_entropy.py
+++ src/cross_entropy.py
@@ -2,12 +2,17 @@ import numpy as np
 
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    # return -np.sum(t * np.log(y)) / batch_size
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
 
 
 def run():
-    t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    t = [2]
     y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
     print(*(zip(t, y)))
     e1 = cross_entropy_error(np.array(y), np.array(t))
